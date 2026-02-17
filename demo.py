e99ec9fd9d741f1ae4bb09bc8be47c86806b5399
u-faizan <mianumarzareen@gmail.com>
2026-02-17 16:59:00 +0500

    data inngestion added

--- demo.py
+++ demo.py
@@ -1,5 +1,7 @@
 # below code is to check the logging config
 from src.logger import logging
+from src.pipline.training_pipeline import TrainPipeline
+# from src.exception import MyException
 import sys
 
 # logging.debug("This is a debug message.")
@@ -9,9 +11,12 @@ import sys
 # logging.critical("This is a critical message.")
 
 # below code is to check the exception config
-from src.exception import MyException
+# try:
+#     result = 1 / 0
+# except Exception as e:
+#     raise MyException(e, sys)
 
-try:
-    result = 1 / 0
-except Exception as e:
-    raise MyException(e, sys)
+from src.pipline.training_pipeline import TrainPipeline
+
+pipline = TrainPipeline()
+pipline.run_pipeline()
