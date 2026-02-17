e36aab3bf7e89fd2a232c2334b7a7ec2b2745925
u-faizan <mianumarzareen@gmail.com>
2026-02-17 10:07:38 +0500

    Initial Project stracture setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Umar Faizan",
+    author_email="mianumarzareen@gmail.com",
+    packages=find_packages()
+)
